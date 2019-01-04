9b3f1f067514632ad9181eed74e5e582a8f18d15
Alina <alna.kireeva88@gmail.com>
2019-01-05 00:02:15 +1300

    added myNewSmartListener

--- events.js
+++ events.js
@@ -1,56 +1,50 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
-document.addEventListener('DOMContentLoaded', start)
+document.addEventListener("DOMContentLoaded", start)
 
 function start() {
-    // The first example is done for you. This will change the background colour of the first div
-    // when you mouse over it.
-    one()
-    two()
-    three()
-    four()
+  // The first example is done for you. This will change the background colour of the first div
+  // when you mouse over it.
+  one()
+  two()
+  three()
+  four()
 
-    // Your turn! Create a new function called `two`, then call it from here.
+  // Your turn! Create a new function called `two`, then call it from here.
 }
 
 function one() {
-    // First, we have to find the element:
-    var one = document.getElementById('one')
+  // First, we have to find the element:
+  var one = document.getElementById("one")
 
-    // Next, we add an event listener to it:
-    one.addEventListener('mouseenter', makeBlue)
+  // Next, we add an event listener to it:
+  one.addEventListener("mouseenter", makeBlue)
 
-    // Finally, we add one to make the colour white again
-    one.addEventListener('mouseleave', makeWhite)
+  // Finally, we add one to make the colour white again
+  one.addEventListener("mouseleave", makeWhite)
 }
 
-
 function two() {
+  var two = document.getElementById("two")
 
-    var two = document.getElementById('two')
+  two.addEventListener("mouseenter", makeGreen)
 
-    two.addEventListener('mouseenter', makeGreen)
-
-    two.addEventListener('mouseleave', makeWhite)
+  two.addEventListener("mouseleave", makeWhite)
 }
 
 function three() {
-    var three = document.getElementById('three')
+  var three = document.getElementById("three")
 
-    three.addEventListener('mouseenter', makeOrange)
+  three.addEventListener("mouseenter", makeOrange)
 
-    three.addEventListener('mouseleave', makeWhite)
-
+  three.addEventListener("mouseleave", makeWhite)
 }
 
 function four() {
-    var four = document.getElementById('four')
+  var four = document.getElementById("four")
 
-    four.addEventListener('click', makePurple)
-
-    four.addEventListener('click', makeWhite)
-
+  four.addEventListener("click", myNewSmartListerner)
 }
 // CREATE FUNCTION two HERE
 
@@ -60,33 +54,32 @@ function four() {
 
 // Changes the background color of event's target
 function makeBlue(evt) {
-    evt.target.style.backgroundColor = 'blue'
-}
-
-function makeWhite(evt) {
-    evt.target.style.backgroundColor = 'white'
+  evt.target.style.backgroundColor = "blue"
 }
 
 function makeGreen(evt) {
-    evt.target.style.backgroundColor = 'green'
-}
-
-function makeWhite(evt) {
-    evt.target.style.backgroundColor = 'white'
+  evt.target.style.backgroundColor = "green"
 }
 
 function makeOrange(evt) {
-    evt.target.style.backgroundColor = 'orange'
-}
-
-function makeWhite(evt) {
-    evt.target.style.backgroundColor = 'white'
+  evt.target.style.backgroundColor = "orange"
 }
 
 function makePurple(evt) {
-    evt.target.style.backgroundColor = 'purple'
+  evt.target.style.backgroundColor = "purple"
 }
 
 function makeWhite(evt) {
-    evt.target.style.backgroundColor = 'white'
+  evt.target.style.backgroundColor = "white"
+}
+
+function myNewSmartListerner(evt) {
+  if (evt.target.style.backgroundColor === "white" ||  evt.target.style.backgroundColor == "") {
+    makePurple(evt)
+  }
+  else {
+    if (evt.target.style.backgroundColor === "purple") {
+      makeWhite(evt)
+    }
+  }
 }
